fb6e0052d5cd05df9869ff7b5fd9b4442d851e80
user <nicklas.nielsen@zliide.com>
2024-04-18 16:02:58 +0200

    Changed the html elements object-fit to contain instead of cover, as the cover showed a "zoomed-in" version of the camera feed

--- packages/camera/camera_web/lib/src/camera.dart
+++ packages/camera/camera_web/lib/src/camera.dart
@@ -165,7 +165,7 @@ class Camera {
     videoElement = html.VideoElement();
 
     divElement = html.DivElement()
-      ..style.setProperty('object-fit', 'cover')
+      ..style.setProperty('object-fit', 'contain')
       ..append(videoElement);
 
     ui_web.platformViewRegistry.registerViewFactory(
@@ -642,6 +642,6 @@ class Camera {
       ..pointerEvents = 'none'
       ..width = '100%'
       ..height = '100%'
-      ..objectFit = 'cover';
+      ..objectFit = 'contain';
   }
 }
